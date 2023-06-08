fc395e4b04a036e9c9373b344f00c87721230520
¨AlejoTorres2001¨ <torres.alejo2001@gmail.com>
2023-06-08 16:27:30 +0200

    add[Frontend]: firebase config init

---
+++ frontend/src/firestore.ts
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import {getFirestore} from 'firebase/firestore';
+
+// Your web app's Firebase configuration
+const setConfig = () => {
+  console.log(import.meta.env.VITE_REACT_APP_apiKey)
+    return {
+      apiKey: import.meta.env.VITE_REACT_APP_apiKey,
+      authDomain: import.meta.env.VITE_REACT_APP_authDomain,
+      projectId: import.meta.env.VITE_REACT_APP_projectId,
+      databaseURL: import.meta.env.VITE_REACT_APP_databaseURL,
+      storageBucket: import.meta.env.VITE_REACT_APP_storageBucket,
+      messagingSenderId: import.meta.env.VITE_REACT_APP_messagingSenderId,
+      appId: import.meta.env.VITE_REACT_APP_appId,
+    };
+  }
+
+
+const app = initializeApp(setConfig());
+const db = getFirestore(app);
+
+export {app,db}
