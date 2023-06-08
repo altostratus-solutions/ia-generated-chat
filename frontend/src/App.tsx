44ae7b505cf6d8dbcda57150c8a84ad67b47746d
¨AlejoTorres2001¨ <torres.alejo2001@gmail.com>
2023-06-08 17:13:27 +0200

    add[Frontend]: binding markdown with state & handlers

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,6 @@
+import './styles/App.css';
+import {db} from './firestore';
+import { addDoc, collection} from "firebase/firestore";
 import { useState } from 'react';
 import { InputOutputTextPair } from './models';
 function App() {
@@ -35,12 +38,12 @@ function App() {
         <h1>Customize your chatbot</h1>
       </header>
         <section>
-          <form className="form" action="">
+          <form className="form" onSubmit={handleCreateChatBot}>
             <label htmlFor="chatbotName">Chatbot Name</label>
-            <input type="text" name="chatbotName" id="chatbotName" />
+            <input value={chatbotName} onChange={(e)=> setChatbotName(e.target.value)} type="text" name="chatbotName" id="chatbotName" />
 
             <label htmlFor="chatbotContext">Chatbot Context</label>
-            <textarea name="chatbotContext" id="chatbotContext" />
+            <textarea value={chatbotContext} onChange={(e)=> setChatbotContext(e.target.value)} name="chatbotContext" id="chatbotContext" />
             <h2>Example Q&A's</h2>
             <article className="examples-container">
               <div className="examples-inputs-container">
@@ -50,6 +53,8 @@ function App() {
                 type="text"
                 name="chatbotExamplesQuestion"
                 id="chatbotExamples"
+                value={chatbotQuestion}
+                onChange={(e)=> setChatbotQuestion(e.target.value)}
               />
 
               <label htmlFor="chatbotExamplesAnswer">Answer:</label>
@@ -57,11 +62,24 @@ function App() {
                 type="text"
                 name="chatbotExamplesAnswer"
                 id="chatbotResponse"
+                value={chatbotResponse}
+                onChange={(e)=> setChatbotResponse(e.target.value)}
               />
-              <button>Load Example</button>
+              <button onClick={handleCreateExampleQuestion}>Load Example</button>
               </div>
               <div className="loaded-examples-container">
                 <h3>Loaded Examples</h3>
+                <ul>
+                  {chatbotExamplesList.map((example, index) => {
+                    return (
+                      <li key={index}>
+                        <p>{example.inputText}</p>
+                        <p>{example.outputText}</p>
+                      </li>
+                    )
+                  }
+                  )}
+                </ul>
               </div>
 
             </article>
