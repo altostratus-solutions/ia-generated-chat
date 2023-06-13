6453e1b662e5be6ff9fcbf398aa4a928c05f87ed
¨AlejoTorres2001¨ <torres.alejo2001@gmail.com>
2023-06-13 11:17:20 +0200

    add[Frontend]: axios public instance

---
+++ frontend/src/api/axios.ts
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_REACT_APP_DEV_API_URL, 
+  headers: {
+    'Content-Type': 'application/json',
+    // Add any custom headers you need
+  },
+});
+
+export default axiosInstance;
