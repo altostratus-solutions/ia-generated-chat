2e7816e842a0100f44367ef8305cb84693bfe4a4
¨AlejoTorres2001¨ <torres.alejo2001@gmail.com>
2023-06-13 16:18:58 +0200

    add[Frontend]: feat. smooth scroll to last message

--- frontend/src/components/Chat.tsx
+++ frontend/src/components/Chat.tsx
@@ -1,10 +1,24 @@
+import { useEffect, useRef } from 'react';
 import { Message } from './ChatModal';
 
 const Chat = ({ messages }:{messages:Message[]
 }) => {
-  
+  const scrollViewRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    scrollToLastMessage();
+  }, [messages]);
+
+  const scrollToLastMessage = () => {
+    if (scrollViewRef.current) {
+      const lastMessage = scrollViewRef.current.lastElementChild;
+      if (lastMessage) {
+        lastMessage.scrollIntoView({ behavior: 'smooth' });
+      }
+    }
+  };
   return (
-    <div className="chat">
+    <div className="chat" ref={scrollViewRef}>
       {messages.map((message, index) => (
         <div key={index} className={`message ${message.isBot ? 'isBot' : 'isUser'}`}>
           {message.message}
