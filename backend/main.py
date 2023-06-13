b14f06339c33baa557b445e440d4ffbdded061d4
¨AlejoTorres2001¨ <torres.alejo2001@gmail.com>
2023-06-13 11:16:50 +0200

    add[Backend]: cors temporary fix

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,25 @@
 from fastapi import FastAPI, Depends, Request
+from fastapi.middleware.cors import CORSMiddleware
 from typing_extensions import Annotated
 from services import get_settings, predict_large_language_model_sample
 import logging
 import config
 from data.firestore import db
+
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3001",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 PARAMETERS = {
