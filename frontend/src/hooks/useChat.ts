63c46c4f42c28ea60ef2a2cba0f2cb3314e216b8
¨AlejoTorres2001¨ <torres.alejo2001@gmail.com>
2023-06-14 09:20:08 +0200

    change[Frontend]: refactor on useChat hook

--- frontend/src/hooks/useChat.ts
+++ frontend/src/hooks/useChat.ts
@@ -1,6 +1,6 @@
-import { useState } from 'react'
-import { InputOutputTextPair } from '../models';
-import axiosInstance, { TEST_MODEL_API_URL } from '../api/axios';
+import { useState } from "react";
+import { InputOutputTextPair } from "../models";
+import { sendMessage } from "../services/sendMessage";
 export type Message = {
   message: string;
   isBot: boolean;
@@ -10,34 +10,29 @@ type useChatParams = {
   chatbotName: string;
   modelContext: string;
   modelExamples: InputOutputTextPair[];
-}
+};
 
-type TestModelRequestBody = {
-  context: string;
-  chatbotName: string;
-  examples: InputOutputTextPair[];
-  message: {
-    text: string;
-  }};
-export default function useChat({chatbotName,modelContext,modelExamples}:useChatParams) {
+export default function useChat({
+  chatbotName,
+  modelContext,
+  modelExamples,
+}: useChatParams) {
   const [messages, setMessages] = useState<Message[]>([]);
 
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState("");
   const handleSendMessage = () => {
-    setMessage('');
+    setMessage("");
     setMessages([...messages, { message, isBot: false }]);
-    const reqBody:TestModelRequestBody = {
+    const reqBody = {
       context: modelContext,
       chatbotName: chatbotName,
       examples: modelExamples,
-      message:{
-        text: message
-      }
-    }
-    axiosInstance.post(TEST_MODEL_API_URL, reqBody).then((res) => {
-      setMessages(prev => [...prev, { message: res.data.text, isBot: true }]);
-    }).catch((err) => {
-      console.log(err);
+      message: {
+        text: message,
+      },
+    };
+    sendMessage(reqBody).then((res) => {
+      setMessages((prev) => [...prev, { message: res.text, isBot: true }]);
     });
   };
   return {
