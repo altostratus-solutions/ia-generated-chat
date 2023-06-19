76f1856bd8f8c9253bf443fcffc9f8cece2e8ad5
¨AlejoTorres2001¨ <torres.alejo2001@gmail.com>
2023-06-19 09:32:38 +0200

    change: removed unused imports

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -3,10 +3,6 @@ import "minireset.css";
 import "../src/styles/tokens.css";
 import "../src/styles/index.css";
 import "../src/styles/App.css";
-import "../src/styles/Input.css";
-import "../src/styles/Modal.css";
-import "../src/styles/ChatModal.css";
-import "../src/styles/Card.css";
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
+import './styles/index.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './styles/index.css'
-import './styles/ChatModal.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
