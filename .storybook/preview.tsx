e706dddfb9493f773ccf679e1abae3a2ebd1fcf7
¨AlejoTorres2001¨ <torres.alejo2001@gmail.com>
2023-06-19 14:09:51 +0200

    remove unused import

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -2,7 +2,6 @@ import type { Preview } from "@storybook/react";
 import "minireset.css";
 import "../src/styles/tokens.css";
 import "../src/styles/index.css";
-import "../src/styles/App.css";
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
