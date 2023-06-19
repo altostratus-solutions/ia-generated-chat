a52848c345b3e206f4c7197a2187ad3714485b98
¨AlejoTorres2001¨ <torres.alejo2001@gmail.com>
2023-06-19 14:03:56 +0200

    add:classname dep & css modules config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    modules: {
+      generateScopedName: '[local]___[hash:base64:2]',
+    },
+  },
   server: {
     watch: {
       usePolling: true,
