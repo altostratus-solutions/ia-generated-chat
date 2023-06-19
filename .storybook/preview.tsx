4d2d2816280590637f3af1759d829c4af11518db
¨AlejoTorres2001¨ <torres.alejo2001@gmail.com>
2023-06-19 17:01:07 +0200

    add: global styles decorators

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,7 +1,28 @@
 import type { Preview } from "@storybook/react";
+import React from "react";
 import "minireset.css";
 import "../src/styles/tokens.css";
 import "../src/styles/index.css";
+
+const styles = {
+  display: "flex",
+  flexDirection: "column",
+  maxHeight: "auto",
+  justifyContent: "flex-start",
+  alignContent: "flex-start",
+  flexWrap: "wrap",
+  height: "100%",
+  gap: "10px 30px" 
+} as const;
+
+  export const decorators = [
+    (Story) => (
+      <div style={styles}>
+        <Story />
+      </div>
+    ),
+  ];
+
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
