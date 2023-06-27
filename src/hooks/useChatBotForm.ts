835910fafe27174876a6cee9662ea7a0859f50e8
¨AlejoTorres2001¨ <torres.alejo2001@gmail.com>
2023-06-27 13:02:37 +0200

    change: replace reducer with useForm
    divided app state

--- src/hooks/useChatBotForm.ts
+++ src/hooks/useChatBotForm.ts
@@ -1,176 +1,49 @@
-import { useReducer } from "react";
-import { ChatBotFormState } from "../models";
 import { addDoc, collection } from "firebase/firestore";
+import {ChatBotForm, chatBotFromSchema } from "../schemas";
 import { CHATBOT_COLLECTION, db } from "../firestore";
-import { v4 as uuidv4 } from "uuid";
-const initialState: ChatBotFormState = {
-  chatbotName: "",
-  modelContext: "",
-  modelExamples: [],
-  currentExample: {
-    inputText: "",
-    outputText: "",
-    id: "",
-  },
-};
-
-export enum ACTIONS {
-  SET_CURRENT_EXAMPLE_QUESTION = "SET_CURRENT_EXAMPLE_QUESTION",
-  SET_CURRENT_EXAMPLE_ANSWER = "SET_CURRENT_EXAMPLE_ANSWER",
-  SET_CHATBOT_NAME = "SET_CHATBOT_NAME",
-  SET_MODEL_CONTEXT = "SET_MODEL_CONTEXT",
-  ADD_MODEL_EXAMPLE = "ADD_MODEL_EXAMPLE",
-  DELETE_MODEL_EXAMPLE = "DELETE_MODEL_EXAMPLE",
-  SEND_FORM = "SEND_FORM",
-}
-
-function chatBotFormReducer(state: ChatBotFormState, action: Action<ACTIONS>) {
-  switch (action.type) {
-    case ACTIONS.SET_CURRENT_EXAMPLE_QUESTION: {
-      return {
-        ...state,
-        currentExample: {
-          ...state.currentExample,
-          inputText: action.payload,
-        },
-      };
-    }
-    case ACTIONS.SET_CURRENT_EXAMPLE_ANSWER: {
-      return {
-        ...state,
-        currentExample: {
-          ...state.currentExample,
-          outputText: action.payload,
-        },
-      };
-    }
-    case ACTIONS.SET_CHATBOT_NAME: {
-      return {
-        ...state,
-        chatbotName: action.payload,
-      };
-    }
-    case ACTIONS.SET_MODEL_CONTEXT: {
-      return {
-        ...state,
-        modelContext: action.payload,
-      };
-    }
-    case ACTIONS.ADD_MODEL_EXAMPLE: {
-      return {
-        ...state,
-        modelExamples: [
-          {
-            inputText: state.currentExample.inputText,
-            outputText: state.currentExample.outputText,
-            id: uuidv4(),
-          },
-          ...state.modelExamples,
-        ],
-        currentExample: initialState.currentExample,
-      };
-    }
-
-    case ACTIONS.DELETE_MODEL_EXAMPLE: {
-      return {
-        ...state,
-        modelExamples: state.modelExamples.filter(
-          (example) => example.id !== action.payload
-        ),
-      };
-    }
-    case ACTIONS.SEND_FORM: {
-      return {
-        ...initialState,
-      };
-    }
-    default: {
-      throw new Error(`Unhandled action: ${action}`);
-    }
-  }
-}
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 function useChatBotForm() {
-  const [state, dispatch] = useReducer<
-    (state: ChatBotFormState, action: Action<ACTIONS>) => ChatBotFormState
-  >(chatBotFormReducer, initialState);
-
-  const handleCreateExampleQuestion = (
-    e: React.MouseEvent<HTMLButtonElement>
-  ) => {
-    e.preventDefault();
+  const {
+    register,
+    handleSubmit,
+    control,
+    getValues,
+    formState: { errors },
+  } = useForm<ChatBotForm>({
+    resolver: zodResolver(chatBotFromSchema),
+  });
+  const createChatBot = async ({
+    chatbotName,
+    modelContext,
+    modelExamples,
+  }:ChatBotForm) => {
     if (
-      state.currentExample.inputText === "" ||
-      state.currentExample.outputText === ""
-    )
-      return;
-    dispatch({
-      type: ACTIONS.ADD_MODEL_EXAMPLE,
-    });
-  };
-  const handleCreateChatBot = async (
-    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
-  ) => {
-    e.preventDefault();
-    if (
-      state.chatbotName === "" ||
-      state.modelContext === "" ||
-      state.modelExamples.length === 0
+      chatbotName === "" ||
+      modelContext === "" ||
+      modelExamples.length === 0
     )
       return;
     try {
       const chatbotCollectionRef = collection(db, CHATBOT_COLLECTION);
       await addDoc(chatbotCollectionRef, {
-        chatbotName: state.chatbotName,
-        modelContext: state.modelContext,
-        modelExamples: state.modelExamples,
+        chatbotName: chatbotName,
+        modelContext: modelContext,
+        modelExamples: modelExamples,
       });
     } catch (error) {
       console.error(error);
     }
-    dispatch({
-      type: ACTIONS.SEND_FORM,
-    });
-  };
-
-  const handleDeleteExample = (id: string) => {
-    dispatch({
-      type: ACTIONS.DELETE_MODEL_EXAMPLE,
-      payload: id,
-    });
-  };
-
+  }
   return {
-    state,
-    dispatch,
-    handleCreateExampleQuestion,
-    handleCreateChatBot,
-    handleDeleteExample,
-  };
+    registerChatBotInput: register,
+    handleSubmitChatBot: handleSubmit,
+    chatBotFormErrors: errors,
+    chatBotFormControl: control,
+    getChatBotFormValues: getValues,
+    createChatBot,
+  }
 }
 
-export default useChatBotForm;
-
-type ActionsWithoutPayload = ACTIONS.ADD_MODEL_EXAMPLE | ACTIONS.SEND_FORM;
-
-interface ActionWithoutPayload {
-  type: ActionsWithoutPayload;
-}
-interface ActionsPayloads {
-  [ACTIONS.SET_CURRENT_EXAMPLE_ANSWER]: string;
-  [ACTIONS.SET_CURRENT_EXAMPLE_QUESTION]: string;
-  [ACTIONS.SET_CHATBOT_NAME]: string;
-  [ACTIONS.SET_MODEL_CONTEXT]: string;
-  [ACTIONS.ADD_MODEL_EXAMPLE]: never;
-  [ACTIONS.SEND_FORM]: never;
-  [ACTIONS.DELETE_MODEL_EXAMPLE]: string;
-}
-
-interface ActionWithPayload<T extends ACTIONS> {
-  type: T;
-  payload: ActionsPayloads[T];
-}
-
-type Action<T extends ACTIONS> = T extends ActionsWithoutPayload
-  ? ActionWithoutPayload
-  : ActionWithPayload<T>;
+export default useChatBotForm
