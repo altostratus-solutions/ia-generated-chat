42f999a00e6e1dbd6302d7aa821282971d9d26bd
¨AlejoTorres2001¨ <torres.alejo2001@gmail.com>
2023-06-27 13:04:31 +0200

    change: schemas & types export pattern

---
+++ src/schemas/index.ts
@@ -0,0 +1,4 @@
+export { questionSchema, questionsFormSchema } from "./question";
+export type { Question, QuestionsForm } from "./question";
+export { chatBotDocSchema, chatBotFromSchema } from "./chatBot";
+export type { ChatBotDoc, ChatBotForm } from "./chatBot";
